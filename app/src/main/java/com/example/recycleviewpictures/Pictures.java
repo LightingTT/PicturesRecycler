bd237643d0b5844d68c713f936ec2d469e2b834c
Piotr Zaremba <qizard@gmail.com>
2020-10-14 17:19:34 +0200

    Update Pictures.java

--- app/src/main/java/com/example/recycleviewpictures/Pictures.java
+++ app/src/main/java/com/example/recycleviewpictures/Pictures.java
@@ -1,6 +1,11 @@
 package com.example.recycleviewpictures;
 import com.google.gson.annotations.SerializedName;
 
+/*
+ * Class responsible for building objects from deserialized data.
+ *
+ */
+
 public class Pictures {
 
     @SerializedName("picture")
