6aebe49ae6155f1cd4f5650d940373c161ee9b95
Piotr Zaremba <qizard@gmail.com>
2020-10-14 17:16:08 +0200

    Update MyService.java

--- app/src/main/java/com/example/recycleviewpictures/MyService.java
+++ app/src/main/java/com/example/recycleviewpictures/MyService.java
@@ -10,5 +10,6 @@ public interface MyService {
 
     //Co tutaj jest moim endpointem? Dla picsum.photos
     @GET("")
+    //Mam zagniezdzoną tutaj klase z danymi, ktore chce pobrac. Czy klasa "Pictures" jest w ogole tutaj potrzebna?
     Call<ArrayList<Pictures>> getFile();
 }
