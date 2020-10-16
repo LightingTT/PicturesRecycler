a1813fcb18f0d1d767ae896ca0cce5685c6ec55c
Piotr Zaremba <qizard@gmail.com>
2020-10-16 17:59:56 +0200

    build fixes

--- app/src/main/java/com/example/recycleviewpictures/ApiClient.java
+++ app/src/main/java/com/example/recycleviewpictures/ApiClient.java
@@ -4,12 +4,13 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    public static String BASE_URL = "https://picsum.photos/200";
+    public static String BASE_URL = "https://picsum.photos/200/";
     private static Retrofit retrofit;
     public static Retrofit getClient(){
     if(retrofit == null){
         retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
                 .build();
     }
         return retrofit;
