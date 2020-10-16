55f2e8a73952b91ab6df37933a46283e762b710f
Piotr Zaremba <qizard@gmail.com>
2020-10-16 17:37:53 +0200

    apiSErvice name change

---
+++ app/src/main/java/com/example/recycleviewpictures/ApiService.java
@@ -0,0 +1,12 @@
+package com.example.recycleviewpictures;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface ApiService {
+
+    @GET("200/")
+    Call<List> getFile();
+}
