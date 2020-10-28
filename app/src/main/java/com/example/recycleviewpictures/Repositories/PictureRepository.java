0ec57c41457d87933bd98549704efd17d78763df
Piotr Zaremba <qizard@gmail.com>
2020-10-28 11:31:48 +0100

    repository for PictureViewListModel

--- app/src/main/java/com/example/recycleviewpictures/Repositories/PictureRepository.java
+++ app/src/main/java/com/example/recycleviewpictures/Repositories/PictureRepository.java
@@ -3,8 +3,29 @@
  */
 
 package com.example.recycleviewpictures.Repositories;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import com.example.recycleviewpictures.Requests.Responsnes.Pictures;
+import java.util.List;
 
 public class PictureRepository {
+    private static PictureRepository instance;
+    private MutableLiveData<List<Pictures>> pictures;
 
+    public static PictureRepository getInstance()
+    {
+        if(instance == null)
+        {
+            instance = new PictureRepository();
+        }
+        return instance;
+    }
 
+    public PictureRepository(){
+        pictures = new MutableLiveData<>();
+    }
+
+    public LiveData<List<Pictures>> getPictures(){
+        return pictures;
+    }
 }
--- app/src/main/java/com/example/recycleviewpictures/ViewModels/PictureListViewModel.java
+++ app/src/main/java/com/example/recycleviewpictures/ViewModels/PictureListViewModel.java
@@ -3,7 +3,7 @@ package com.example.recycleviewpictures.ViewModels;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
-
+import com.example.recycleviewpictures.Repositories.PictureRepository;
 import com.example.recycleviewpictures.Requests.Responsnes.Pictures;
 
 import java.util.List;
@@ -11,17 +11,16 @@ import java.util.List;
 public class PictureListViewModel extends ViewModel {
 
     private MutableLiveData<List<Pictures>> pictureList = new MutableLiveData<>();
+    private PictureRepository pictureRepository;
 
-    public PictureListViewModel(MutableLiveData<List<Pictures>> pictureList) {
-        this.pictureList = pictureList;
-    }
+
     public PictureListViewModel() {
-
+    pictureRepository = PictureRepository.getInstance();
     }
 
     //immutable. Can be observed and changed indirectly.
     public LiveData<List<Pictures>> getPictures(){
-        return pictureList;
+        return pictureRepository.getPictures();
     }
 
 
